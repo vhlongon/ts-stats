f79bafeefbe1dfdaff4cc27c84dd6bf4f52731ee
victor <vhlongon@gmail.com>
2021-01-10 22:43:56 +0100

    Create function to print total number of wins for a hardcoded team

--- build/index.js
+++ build/index.js
@@ -3,7 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 var matches = fs_1.readFileSync('football.csv', { encoding: 'utf-8' })
     .split('\n')
-    .map(function (row) {
-    return row.split(',');
-});
-console.log(matches);
+    .map(function (row) { return row.split(','); });
+// enum - enumaration - used for closely related values
+// when we have a small fixed set of values known at compile time
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var winsForManUnited = matches.reduce(function (acc, val) {
+    var homeTeam = val[1], awayTeam = val[2], winner = val[5];
+    var winnerHome = homeTeam === 'Man United' && winner === MatchResult.HomeWin;
+    var winnerAway = awayTeam === 'Man United' && winner === MatchResult.AwayWin;
+    return winnerHome || winnerAway ? acc + 1 : acc;
+}, 0);
+console.log("Man United won " + winsForManUnited + " games");
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,23 @@ import { readFileSync } from 'fs';
 
 const matches = readFileSync('football.csv', { encoding: 'utf-8' })
   .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+  .map((row: string): string[] => row.split(','));
 
-console.log(matches);
+// enum - enumaration - used for closely related values
+// when we have a small fixed set of values known at compile time
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+const winsForManUnited = matches.reduce((acc, val) => {
+  const [, homeTeam, awayTeam, , , winner] = val;
+  const winnerHome =
+    homeTeam === 'Man United' && winner === MatchResult.HomeWin;
+  const winnerAway =
+    awayTeam === 'Man United' && winner === MatchResult.AwayWin;
+  return winnerHome || winnerAway ? acc + 1 : acc;
+}, 0);
+
+console.log(`Man United won ${winsForManUnited} games`);
