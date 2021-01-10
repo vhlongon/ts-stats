5268c85c33e63cba968452dbe171ac3c8b23c0ee
victor <vhlongon@gmail.com>
2021-01-10 22:57:33 +0100

    make read file and calculate wins for team as reusable functions

--- build/index.js
+++ build/index.js
@@ -1,9 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
-var matches = fs_1.readFileSync('football.csv', { encoding: 'utf-8' })
-    .split('\n')
-    .map(function (row) { return row.split(','); });
+var readFile = function (fileName) {
+    return fs_1.readFileSync(fileName, { encoding: 'utf-8' })
+        .split('\n')
+        .map(function (row) { return row.split(','); });
+};
+var matches = readFile('football.csv');
 // enum - enumaration - used for closely related values
 // when we have a small fixed set of values known at compile time
 var MatchResult;
@@ -12,10 +15,13 @@ var MatchResult;
     MatchResult["AwayWin"] = "A";
     MatchResult["Draw"] = "D";
 })(MatchResult || (MatchResult = {}));
-var winsForManUnited = matches.reduce(function (acc, val) {
-    var homeTeam = val[1], awayTeam = val[2], winner = val[5];
-    var winnerHome = homeTeam === 'Man United' && winner === MatchResult.HomeWin;
-    var winnerAway = awayTeam === 'Man United' && winner === MatchResult.AwayWin;
-    return winnerHome || winnerAway ? acc + 1 : acc;
-}, 0);
+var winsForTeam = function (teamName, data) {
+    return data.reduce(function (acc, val) {
+        var homeTeam = val[1], awayTeam = val[2], winner = val[5];
+        var winnerHome = homeTeam === teamName && winner === MatchResult.HomeWin;
+        var winnerAway = awayTeam === teamName && winner === MatchResult.AwayWin;
+        return winnerHome || winnerAway ? acc + 1 : acc;
+    }, 0);
+};
+var winsForManUnited = winsForTeam('Man United', matches);
 console.log("Man United won " + winsForManUnited + " games");
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { readFileSync } from 'fs';
 
-const matches = readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const readFile = (fileName: string): string[][] =>
+  readFileSync(fileName, { encoding: 'utf-8' })
+    .split('\n')
+    .map((row: string): string[] => row.split(','));
+
+const matches = readFile('football.csv');
 
 // enum - enumaration - used for closely related values
 // when we have a small fixed set of values known at compile time
@@ -12,16 +15,14 @@ enum MatchResult {
   Draw = 'D',
 }
 
-const winsForManUnited = matches.reduce(
-  (acc: number, val: string[]): number => {
+const winsForTeam = (teamName: string, data: string[][]): number =>
+  data.reduce((acc: number, val: string[]): number => {
     const [, homeTeam, awayTeam, , , winner] = val;
-    const winnerHome =
-      homeTeam === 'Man United' && winner === MatchResult.HomeWin;
-    const winnerAway =
-      awayTeam === 'Man United' && winner === MatchResult.AwayWin;
+    const winnerHome = homeTeam === teamName && winner === MatchResult.HomeWin;
+    const winnerAway = awayTeam === teamName && winner === MatchResult.AwayWin;
     return winnerHome || winnerAway ? acc + 1 : acc;
-  },
-  0
-);
+  }, 0);
+
+const winsForManUnited = winsForTeam('Man United', matches);
 
 console.log(`Man United won ${winsForManUnited} games`);
